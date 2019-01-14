05c0236c7c9a7a3b9f39d3533ed0a9df325ec13f
isonz <isonz@qq.com>
2019-01-14 17:28:46 +0800

    asdf

--- platforms/windows/platform_www/cordova_plugins.js
+++ platforms/windows/platform_www/cordova_plugins.js
@@ -1,9 +1,67 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-hot-code-push-plugin.chcp",
+    "file": "plugins/cordova-hot-code-push-plugin/www/chcp.js",
+    "pluginId": "cordova-hot-code-push-plugin",
+    "clobbers": [
+      "chcp"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.Camera",
+    "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "Camera"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.CameraPopoverOptions",
+    "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "CameraPopoverOptions"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.camera",
+    "file": "plugins/cordova-plugin-camera/www/Camera.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "navigator.camera"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.CameraPopoverHandle",
+    "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "CameraPopoverHandle"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.CameraProxy",
+    "file": "plugins/cordova-plugin-camera/src/windows/CameraProxy.js",
+    "pluginId": "cordova-plugin-camera",
+    "runs": true
+  },
+  {
+    "id": "cordova-hot-code-push-local-dev-addon.chcpLocalDev",
+    "file": "plugins/cordova-hot-code-push-local-dev-addon/www/chcpLocalDev.js",
+    "pluginId": "cordova-hot-code-push-local-dev-addon",
+    "clobbers": [
+      "chcpLocalDev"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-hot-code-push-plugin": "1.5.3",
+  "cordova-plugin-camera": "4.0.3",
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-hot-code-push-local-dev-addon": "0.4.2"
 };
 // BOTTOM OF METADATA
 });
